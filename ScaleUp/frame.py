23a192717c881bfbbe3e6431d46b00777a7d7408
akbli <ar@sem-wolf.org>
2020-09-09 13:30:17 +0300

    frame department for representing a single traffic light frame

---
+++ ScaleUp/frame.py
@@ -0,0 +1,9 @@
+class Frame:
+    def __init__(self):
+        self.frame_id = 0
+        self.frame_path = ""
+        self.light_candidates = []
+        self.tfl_candidates = []
+        self.light_auxiliary = []
+        self.tfl_auxiliary = []
+        self.tfl_distance = []
