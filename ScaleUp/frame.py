6276ccee3e0c2426d0e628fe0428c0bfa5b5f357
Ruthie Eghbali <ar@sem-wolf.org>
2021-01-08 10:19:12 -0800

    remove ScaleUp folder

--- ScaleUp/frame.py
+++
@@ -1,9 +0,0 @@
-class Frame:
-    def __init__(self):
-        self.frame_id = 0
-        self.frame_path = ""
-        self.light_candidates = []
-        self.tfl_candidates = []
-        self.light_auxiliary = []
-        self.tfl_auxiliary = []
-        self.tfl_distance = []
