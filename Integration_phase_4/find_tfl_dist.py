91f3b16e9384313b95aa278234ab743279efe533
Ruth Eqbali <rutheghbali@gmail.com>
2021-01-10 12:40:41 -0800

    main of the integration phase on the project

---
+++ Integration_phase_4/find_tfl_dist.py
@@ -0,0 +1,10 @@
+from Integration_phase_4.controller import Controller
+
+
+def main():
+    controller = Controller('data/frames.pls')
+    controller.run()
+
+
+if __name__ == '__main__':
+    main()
